98ee98630431907435f3a97c44ab51f1efff6764
Majun1990 <merijun_a@hotmail.com>
2019-09-17 21:18:20 +0000

    added gsub

--- challenge1/lib/challenge1.rb
+++ challenge1/lib/challenge1.rb
@@ -1,16 +1,21 @@
 class Challenge1
   def self.balanced?(string)
+    array = string.gsub(/(\w)(\d)/, "")
     pairs = { '(' => ')', '{' => '}', '[' => ']'}
     stack = []
-    string.each_char do |items|
-      if items == pairs.keys
-        stack << items
-      elsif items == pairs.values
-        stack.pop
-        true
-      else
-        false
+      array.each_char do |items| #iterate to each character of array
+        if items == pairs.keys       # any of the item is equal to hash pairs keys, items go in array stack
+          stack << items
+          if items == pairs.values   # any of the item is equal to hash pairs value, the item that pairs it, gets taken out of stack
+            stack.pop
+            if stack.empty?          # evaluate if stack is empty, which means that
+              true                     
+            else
+              false
+            end
+          end
+        end
       end
-    end
+
   end
 end
