37284d948f16992650a643f44f122ce165f7e613
Majun1990 <merijun_a@hotmail.com>
2019-09-19 16:22:49 +0000

    1 failure

--- challenge1/lib/challenge1.rb
+++ challenge1/lib/challenge1.rb
@@ -1,21 +1,22 @@
 class Challenge1
   def self.balanced?(string)
-    array = string.gsub(/(\w)(\d)/, "")
-    pairs = { '(' => ')', '{' => '}', '[' => ']'}
+    pairs = { ')' => '(', '}' => '{', ']' => '['}
+    opening_char = ['(', '[', '{']
     stack = []
-      array.each_char do |items| #iterate to each character of array
-        if items == pairs.keys       # any of the item is equal to hash pairs keys, items go in array stack
-          stack << items
-          if items == pairs.values   # any of the item is equal to hash pairs value, the item that pairs it, gets taken out of stack
-            stack.pop
-            if stack.empty?          # evaluate if stack is empty, which means that
-              true                     
-            else
-              false
-            end
-          end
+      string.each_char do |item| #iterate to each character of array
+        if opening_char.include?(item)
+          stack.push(item)
+        elsif pairs[item] == stack.last
+          stack.pop
+        else
+          return false
         end
+  
       end
-
-  end
+      if stack.empty?
+        true
+      else
+        false 
+      end
+   end
 end
--- challenge2/lib/challenge2.rb
+++ challenge2/lib/challenge2.rb
@@ -1,5 +1,6 @@
 class Challenge2
   def self.sum_to_0(array)
-    array.inject(3){|sum, n| sum + n} == 0
+    array.each do |number|
+      if number.inject(3){|sum, n| sum + n} == 0
   end
 end
