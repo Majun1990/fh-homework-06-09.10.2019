19968b0856dc82815a873340f9703cda48f76ec5
Majun1990 <merijun_a@hotmail.com>
2019-09-19 23:32:40 +0000

    added closing_char

--- challenge1/lib/challenge1.rb
+++ challenge1/lib/challenge1.rb
@@ -2,16 +2,20 @@ class Challenge1
   def self.balanced?(string)
     pairs = { ')' => '(', '}' => '{', ']' => '['}
     opening_char = ['(', '[', '{']
+    closing_char = [')', ']', '}']
     stack = []
       string.each_char do |item| #iterate to each character of array
-        if opening_char.include?(item)
+        if opening_char.include?(item)  #if it is an opening character, put in the stack
           stack.push(item)
-        elsif pairs[item] == stack.last
-          stack.pop
-        else
-          return false
+        else closing_char.include?(item) #check if next character is a closing char
+          if stack.empty?
+            return false
+          elsif closing_char != stack.last
+            return false
+          else closing_char == stack.last                  # if it is a match, pop the last character from the stack
+            stack.pop(item)
+          end                  
         end
-  
       end
       if stack.empty?
         true
